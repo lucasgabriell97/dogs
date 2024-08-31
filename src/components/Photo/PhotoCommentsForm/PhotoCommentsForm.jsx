6cb7fdef543bad1f0d7bfea1759c3def5ae4efec
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2024-08-31 20:57:15 -0300

    Finalizando modal da foto e o post  de comentários

---
+++ src/components/Photo/PhotoCommentsForm/PhotoCommentsForm.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Enviar from "../../../assets/images/enviar.svg?react";
+import useFetch from "../../../hooks/useFetch/useFetch";
+import { COMMENT_POST } from "../../../services/api";
+import Error from "../../Error/Error";
+
+const PhotoCommentsForm = ({ id, setComments }) => {
+  const [comment, setComment] = React.useState("");
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const token = window.localStorage.getItem("token");
+    const { url, options } = COMMENT_POST(id, { comment }, token);
+    const { response, json } = await request(url, options);
+
+    if (response.ok) {
+      setComment("");
+      setComments((comments) => [...comments, json]);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import styles from "./Home.module.css"
+import Feed from '../../components/Feed/Feed'
 
 const Home = () => {
   return (
-    <div className={styles.home}>Home</div>
+    <section className="container mainContainer"><Feed /></section>
   )
 }
 
