a324e3bcb52f8023e21718dc9c60e6a21b7f5456
Lucas Gabriell <lucas.gabriell97@hotmail.com>
2024-09-02 20:55:18 -0300

    Criando o método para deletar a foto

--- src/components/Photo/PhotoContent/PhotoContent.jsx
+++ src/components/Photo/PhotoContent/PhotoContent.jsx
@@ -2,18 +2,27 @@ import React from "react";
 import styles from "./PhotoContent.module.css";
 import { Link } from "react-router-dom";
 import PhotoComments from "../PhotoComments/PhotoComments";
+import { UserContext } from "../../../UserContext";
+import PhotoDelete from "../PhotoDelete/PhotoDelete";
+import Image from "../../../helper/Image/Image";
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
+
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.previews}>{photo.acessos}</span>
           </p>
           <h1 className="title">
